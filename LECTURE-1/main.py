8be7ba7264b75d95a67d96ad83fa7e9c857c94ee
anagha zachariah c <anaghazachariah@gmail.com>
2021-04-17 17:34:11 +0530

    update

--- LECTURE-1/main.py
+++ LECTURE-1/main.py
@@ -1,7 +1,15 @@
-from flask import Flask
-app=Flask(__name__)
-@app.route("/")
+'''
+In this program program we are creating a simple webpage,which displays "HELLO"
+
+Run the application.Then open a browser and type in the URL http://127.0.0.1:5000/   ,
+you will receive the string HELLO as a response, this confirms
+that your application is successfully running.
+'''
+from flask import Flask #import the Flask object from the flask package
+app=Flask(__name__)#creating your Flask application instance with the name app
+
+@app.route("/")#pass '/' to signify that this function respond to main URL
 def home():
-    return "HELLO"
+    return "HELLO"#returns string "HELLO"
 if __name__=="__main__":
-    app.run()
+   app.run()#run the development server.
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FLASK_LECTURES
+activate your Python environment and install Flask using the pip package installer
+->pip install flask
